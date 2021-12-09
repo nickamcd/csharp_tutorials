efe53f0cc092aebb8d978d11ac9695a3002aafba
Nick McDaniel <nickamcd@gmail.com>
2021-12-08 22:42:04 -0800

    Made initial build

--- ArraysListsCollections/Program.cs
+++ ArraysListsCollections/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ArraysListsCollections
+{
+  class Program 
+  {
+    static void Main(string[] args)
+    {
+      var names = new List<string> { "<name>", "Ana", "Felipe" };
+      foreach (string name in names)
+      {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
+    }
+  }
+}
