da159910d8b222be5dc1263775c373afdd5005b3
Nick McDaniel <nickamcd@gmail.com>
2021-12-09 14:08:49 -0800

    Practiced sorting the list of strings

--- ArraysListsCollections/Program.cs
+++ ArraysListsCollections/Program.cs
@@ -9,6 +9,29 @@
       {
         Console.WriteLine($"Hello {name.ToUpper()}!");
       }
+
+      names.Add("Tony");
+      names.Add("Maria");
+      names.Add("Bill");
+
+      var index = names.IndexOf("Jeremy");
+      Console.WriteLine($"Found Jeremy at {index}");
+
+      if (index == -1)
+      {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+      }
+      else
+      {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+      }
+
+      names.Sort();
+      foreach (string name in names)
+      {
+        Console.WriteLine($"{name}");
+      }
+
     }
   }
 }
