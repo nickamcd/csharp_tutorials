f56bf21e8b7d60a1495215de1122805dc8776fff
Nick McDaniel <nickamcd@gmail.com>
2021-12-11 20:38:32 -0800

    Added file reading function

--- TeleprompterConsole/Program.cs
+++ TeleprompterConsole/Program.cs
@@ -4,7 +4,19 @@
   {
     static void Main(string[] args)
     {
-      
+
+    }
+
+    static IEnumerable<string> ReadFrom(string file)
+    {
+      string? line;
+      using (var reader = File.OpenText(file))
+      {
+        while ((line = reader.ReadLine()) != null)
+        {
+          yield return line;
+        }
+      }
     }
   }
 }
